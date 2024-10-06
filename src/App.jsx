b0fb336a61e517cbd32b0074feea9833ca745433
Herbert540 <HerbertBotwe2025@u.northwestern.edu>
2024-10-06 09:38:23 -0500

    add firebase database

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import Banner from './components/Banner';
 import TermPage from './components/TermPage';
 import StatusMessage from './components/StatusMessage';
-import { useCourses } from './utilities/fetchCourses';
+import { useDbData } from './firebase';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
@@ -11,9 +11,8 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 const queryClient = new QueryClient();
 
 const App = () => {
-  const { data, error, isLoading } = useCourses();
+  const { data, error, isLoading } = useDbData('courses');
 
-  // Handle loading or error state
   if (isLoading || error) {
     return <StatusMessage message={isLoading ? "Loading..." : `Error fetching courses: ${error.message}`} />;
   }
@@ -21,7 +20,7 @@ const App = () => {
   return (
     <QueryClientProvider client={queryClient}>
       <div>
-        <Banner title={data.title} />
+        <Banner title={data.title || "CS Courses"} />
         <TermPage courses={data.courses} />
       </div>
     </QueryClientProvider>
